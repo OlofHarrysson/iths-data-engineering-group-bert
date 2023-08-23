2a1ff282a214d0f550f3fde4d5787b2d994762d1
Kevin Abrahamsson <kevin.abrahamsson@iths.se>
2023-08-23 13:08:42 +0200

    implemented function to write summaries for a given text.

--- src/newsfeed/summarize.py
+++ src/newsfeed/summarize.py
@@ -1,26 +1,22 @@
 from dotenv import load_dotenv
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import WebBaseLoader
+from langchain.docstore.document import Document
 
 # Load dotenv in order to use the OpenAi API key
 load_dotenv()
 
 
 def summarize_text(blog_text):
-    # Download the text
-    loader = WebBaseLoader(blog_text)
-    docs = loader.load()
+    # Create a document object list for the library
+    docs = [Document(page_content=blog_text)]
 
+    # declare the model with a temperature of 0 in order to maximize conciseness
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
     chain = load_summarize_chain(llm, chain_type="stuff")
 
-    summary = chain.run(docs)
-
-    return summary
+    return chain.run(docs)
 
 
-# Example
-if __name__ == "__main__":
-    summary = summarize_text("https://lilianweng.github.io/posts/2023-06-23-agent/")
-    print(summary)
+summary = summarize_text("test")
+print(summary)
