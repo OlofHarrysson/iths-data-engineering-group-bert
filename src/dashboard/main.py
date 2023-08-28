0eb1a18645b7e7f2644f8d54ead235c41d0aa1a4
JoelOscarsson <j_oscarsson@hotmail.com>
2023-08-28 11:52:51 +0200

    starting architecture for dashboard

---
+++ src/dashboard/main.py
@@ -0,0 +1,10 @@
+# Importing packages/or modules
+# H1 represents a top-level heading.
+# H2 represents a secondary heading.
+# P represents a paragraph.
+# Div represents a generic division or container.
+# Button represents a clickable button.
+from pathlib import Path
+
+from dash import Dash, Input, Output, State, dcc, html
+from dash.html import H1, H2, Button, Div, P
