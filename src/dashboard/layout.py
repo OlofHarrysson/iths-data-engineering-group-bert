d18ddff00c30c7ae80b0835821fc2b055454cb84
JoelOscarsson <j_oscarsson@hotmail.com>
2023-08-28 15:47:52 +0200

    created function and a header

---
+++ src/dashboard/layout.py
@@ -0,0 +1,11 @@
+from dash import Dash, html
+
+
+def create_layout(app: Dash) -> html.Div:
+    return html.Div(
+        className="app-div",
+        children=[
+            html.H1(app.title),
+            html.Hr(),
+        ],
+    )
