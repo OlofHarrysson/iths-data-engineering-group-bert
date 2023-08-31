cc985784d849d1fabcc6384cbe5009ed1c865206
JoelOscarsson <j_oscarsson@hotmail.com>
2023-08-31 15:34:53 +0200

    outcommented test

--- tests/test_local_model.py
+++ tests/test_local_model.py
@@ -1,9 +1,9 @@
 from textsum.summarize import Summarizer
 
-from newsfeed.summarize import summarize_local_model
+# from newsfeed.summarize import summarize_local_model
 
 
-def test_local_model_inference():
-    text = """This is a test"""
-    summary = summarize_local_model(text)
-    print(f"Summary: {summary}")
+# def test_local_model_inference():
+#     text = """This is a test"""
+#     summary = summarize_local_model(text)
+#     print(f"Summary: {summary}")
