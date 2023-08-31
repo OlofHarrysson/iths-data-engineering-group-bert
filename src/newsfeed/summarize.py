303b1cf4a3f81d595ff1e91909fb6d6087d3f51e
Andreas Svensson <andreas.svensson@iths.se>
2023-08-31 13:48:32 +0200

    updated folder structure to prepare for nontech summaries

--- src/newsfeed/summarize.py
+++ src/newsfeed/summarize.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import re
@@ -8,7 +9,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.docstore.document import Document
 
 from newsfeed.datatypes import BlogInfo
-from newsfeed.get_summaries import check_summary_cache, data_directory_path
+from newsfeed.get_cached_files import check_cache, data_directory_path
 
 # Load dotenv in order to use the OpenAi API key
 load_dotenv()
@@ -44,34 +45,40 @@ def read_articles(dir):
 def get_article_directories():
     article_directories = []
 
-    # Make a list of all article directories
-    for dir in os.listdir(data_directory_path):
-        if os.path.isdir(os.path.join(data_directory_path, dir)):
-            article_directories.append(dir + "/articles")
+    # # Make a list of all article directories
+    # for dir in os.listdir(data_directory_path):
+    #     if os.path.isdir(os.path.join(data_directory_path, dir)):
+    #         article_directories.append(dir + "/articles")
 
-    # Remove summaries directory from list, as we don't wanna summarize the summaries
-    try:
-        article_directories.remove("summaries/articles")
-    except:
-        pass
+    # # Remove summaries directory from list, as we don't wanna summarize the summaries
+    # try:
+    #     article_directories.remove("summaries/articles")
+    # except:
+    #     pass
+
+    # NOTE: code not tested properly
+    for dir in os.listdir(os.path.join(data_directory_path, "articles")):
+        article_directories.append(dir)
 
     return article_directories
 
 
-def summarize_articles():
-    # For future kev to deal with.
-    # Fix bug where it fucks up if the articles directory is empty
+def summarize_articles(summary_type):
+    """summarize all articles into summary_type, i.e. tech or nontech, if they are not already summarized"""
 
     article_directories = get_article_directories()
 
     for dir in article_directories:
-        os.makedirs(os.path.dirname(data_directory_path + "/summaries/" + dir + "/"), exist_ok=True)
+        os.makedirs(
+            os.path.dirname(data_directory_path + f"/{summary_type}_summaries/" + dir + "/"),
+            exist_ok=True,
+        )
 
         blogs = read_articles(data_directory_path + dir)
 
         for blog in blogs:
             # Check if the blog summary already exists
-            if not check_summary_cache(blog.unique_id):
+            if not check_cache(blog.unique_id, summary_type):  # TODO: pass in article type
                 # Remove file name characters disallowed by the filesystem
                 file_name = re.sub(r'[\/:*?"<>|]', "", blog.title.replace(" ", "_"))
 
@@ -89,11 +96,28 @@ def summarize_articles():
                 }
 
                 with open(
-                    data_directory_path + "/summaries/" + dir + "/" + file_name + ".json",
+                    data_directory_path
+                    + f"/{summary_type}_summaries/"
+                    + dir
+                    + "/"
+                    + file_name
+                    + ".json",
                     "w",
                 ) as file:
                     json.dump(blog_summary, file, indent=4)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--summary_type",
+        type=str,
+        choices=["tech", "nontech"],
+        help='Choose either "tech" or "nontech" as the type of summary',
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    summarize_articles()
+    args = parse_args()
+    summarize_articles(summary_type=args.summary_type)
