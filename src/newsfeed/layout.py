a8579daf7f2435b3f10b9f0efa7f371f89ad154f
LukasKallstrom <lukas.k0325@gmail.com>
2023-09-04 11:27:47 +0200

    initial reformatt of main and layout

--- src/newsfeed/layout.py
+++ src/newsfeed/layout.py
@@ -1,42 +1,59 @@
 import dash_bootstrap_components as dbc
-from dash import Dash, html
-from get_summaries import get_summaries
+import dash_daq as daq
+from dash import Dash, Input, Output, State, dcc, html
+from dash.dependencies import Input, Output
+
+from newsfeed.get_summaries import get_summaries
+from newsfeed.main import app
+
+
+# Create a function to generate a card component for a summary
+def generate_card(summary):
+    return dbc.Card(
+        dbc.CardBody(
+            [
+                html.H4(summary.title, className="card-title"),
+                html.P(summary.summary),
+                html.P(
+                    f"Published: {summary.published}",
+                    style={"font-size": "12px", "color": "gray"},
+                ),
+                dbc.Button(
+                    "Visit Blog",
+                    color="primary",
+                    href=summary.link,
+                    target="_blank",
+                ),
+            ]
+        )
+    )
 
 
 # The actual layout for the dash app
 def create_layout(app: Dash) -> dbc.CardBody:
-    summaries = get_summaries()
+    # Get initial summaries based on the default value "tech"
+    initial_summaries = get_summaries("tech")
 
-    return dbc.Container(
+    layout = dbc.Container(
         [
-            dbc.Row(
-                dbc.Col(
-                    [
-                        dbc.Card(
-                            # Use list comprehension to loop over all articles and create cards for each
-                            dbc.CardBody(
-                                [
-                                    html.H4(summary.title, className="card-title"),
-                                    html.P(summary.summary),
-                                    html.P(
-                                        f"Published: {summary.published}",
-                                        style={"font-size": "12px", "color": "gray"},
-                                    ),
-                                    dbc.Button(
-                                        "Visit Blog",
-                                        color="primary",
-                                        href=summary.link,
-                                        target="_blank",
-                                    ),
-                                ]
-                            )  # ,
-                            # id="card-title"
-                        )
-                        for summary in summaries  # Create 3 identical cards
-                    ],
-                    width=6,
-                ),
+            daq.ToggleSwitch(
+                label="Toggle switch", labelPosition="bottom", id="my_input", value="tech"
+            ),
+            html.Div(  # Container for card components
+                id="summary-container",
+                children=[generate_card(summary) for summary in initial_summaries],
             ),
         ],
         fluid=True,
     )
+
+    return layout
+
+
+@app.callback(
+    Output("summary-container", "children"),  # Output: summary container's children
+    Input("my_input", "value"),  # Input: toggle switch value
+)
+def update_summaries(value: str):
+    summaries = get_summaries(value)
+    return [generate_card(summary) for summary in summaries]
