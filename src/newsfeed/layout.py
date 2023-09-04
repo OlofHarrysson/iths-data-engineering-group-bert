b8c15bee96a26f8b4e5f5ddc54c59ef26ae1636b
LukasKallstrom <lukas.k0325@gmail.com>
2023-09-04 15:43:20 +0200

    basic funtionality, 2 columns of summaries

--- src/newsfeed/layout.py
+++ src/newsfeed/layout.py
@@ -1,42 +1,72 @@
 import dash_bootstrap_components as dbc
+import dash_daq as daq
 from dash import Dash, html
-from get_summaries import get_summaries
+
+from newsfeed.get_cached_files import get_contents
 
 
 # The actual layout for the dash app
 def create_layout(app: Dash) -> dbc.CardBody:
-    summaries = get_summaries()
+    tech_summaries = get_contents("tech_summaries")
+    non_tech_summaries = get_contents("nontech_summaries")
 
-    return dbc.Container(
+    tech_cards = [
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4(summary.title, className="ccard-title"),
+                    html.P(summary.summary),
+                    html.P(
+                        f"Published: {summary.published}",
+                        style={"font-size": "12px", "color": "gray"},
+                    ),
+                    html.P(
+                        f"Type: Tech",
+                        style={"font-size": "12px", "color": "gray"},
+                    ),
+                    dbc.Button(
+                        "Visit Blog",
+                        color="primary",
+                        href=summary.link,
+                        target="_blank",
+                    ),
+                ]
+            )
+        )
+        for summary in tech_summaries
+    ]
+
+    non_tech_cards = [
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4(summary.title, className="ccard-title"),
+                    html.P(summary.summary),
+                    html.P(
+                        f"Published: {summary.published}",
+                        style={"font-size": "12px", "color": "gray"},
+                    ),
+                    html.P(
+                        f"Type: Nontech",
+                        style={"font-size": "12px", "color": "gray"},
+                    ),
+                    dbc.Button(
+                        "Visit Blog",
+                        color="primary",
+                        href=summary.link,
+                        target="_blank",
+                    ),
+                ]
+            )
+        )
+        for summary in non_tech_summaries
+    ]
+
+    return dbc.Row(
         [
-            dbc.Row(
-                dbc.Col(
-                    [
-                        dbc.Card(
-                            # Use list comprehension to loop over all articles and create cards for each
-                            dbc.CardBody(
-                                [
-                                    html.H4(summary.title, className="card-title"),
-                                    html.P(summary.summary),
-                                    html.P(
-                                        f"Published: {summary.published}",
-                                        style={"font-size": "12px", "color": "gray"},
-                                    ),
-                                    dbc.Button(
-                                        "Visit Blog",
-                                        color="primary",
-                                        href=summary.link,
-                                        target="_blank",
-                                    ),
-                                ]
-                            )  # ,
-                            # id="card-title"
-                        )
-                        for summary in summaries  # Create 3 identical cards
-                    ],
-                    width=6,
-                ),
+            dbc.Col(tech_cards, style={"width": "40%", "inline-block": "True", "height": "100px"}),
+            dbc.Col(
+                non_tech_cards, style={"width": "40%", "inline-block": "True", "height": "100px"}
             ),
-        ],
-        fluid=True,
+        ]
     )
