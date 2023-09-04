344cd6279bb1acce83931153917340cad93d4fb6
Andreas Svensson <andreas.svensson@iths.se>
2023-09-04 22:26:28 +0200

    added function to extract source from summaries

--- src/newsfeed/main.py
+++ src/newsfeed/main.py
@@ -68,26 +68,33 @@ def sort_summaries(summaries):
     return sorted_summaries
 
 
+def get_source(summary):
+    parsed_url = urlparse(summary.link)
+    source = parsed_url.netloc
+
+    return source
+
+
 def get_summary_by_source(summaries):
     summaries_dict = {}
 
     for summary in summaries:
-        source = summary.source
+        source = get_source(summary)
 
         if source not in summaries_dict:
             summaries_dict[source] = []
 
-        summaries_dict[summary.source].append(summary)
+        summaries_dict[source].append(summary)
 
     return summaries_dict
 
 
-def amount_summaries_from_each_source(summaries, n=5):
+def amount_summaries_from_each_source(summaries, n=10):
     top_summaries = []
     summaries_dict = get_summary_by_source(summaries)
 
     for source in summaries_dict:
-        top_summaries.append(summaries_dict[source][:n:])
+        top_summaries.extend(summaries_dict[source][:n])
 
     return top_summaries
 
