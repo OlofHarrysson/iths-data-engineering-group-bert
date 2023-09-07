2891d9f739265fbbdd0439407f50711223c7833c
Kevin Abrahamsson <kevin.abrahamsson@iths.se>
2023-09-07 09:19:41 +0200

    working?

---
+++ dags/run_pipeline.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+
+from newsfeed import download_blogs_from_rss, extract_articles, summarize
+
+
+@task(task_id="download_blogs_from_rss_task")
+def download_blogs_from_rss_task() -> None:
+    download_blogs_from_rss.main("mit")
+    download_blogs_from_rss.main("sd")
+    download_blogs_from_rss.main("openai")
+
+
+@task(task_id="extract_articles_task")
+def extract_articles_task() -> None:
+    extract_articles.main("mit")
+    extract_articles.main("sd")
+    extract_articles.main("openai")
+
+
+@task(task_id="summarize_task")
+def summarize_task() -> None:
+    summarize.summarize_articles(summary_type="nontech", model_type="api")
+    summarize.summarize_articles(summary_type="tech", model_type="api")
+    # summarize.summarize_articles("openai")
+
+
+@task(task_id="send_to_discord_task")
+def send_to_discord_task() -> None:
+    # Implement
+    pass
+
+
+# Create the dag pipeline
+@dag(
+    dag_id="run_pipeline",
+    start_date=datetime(2023, 6, 2),
+    schedule_interval=None,
+    catchup=False,
+)
+def test_pipeline() -> None:
+    # Run the scripts in this order
+    (
+        download_blogs_from_rss_task()
+        >> extract_articles_task()
+        >> summarize_task()
+        >> send_to_discord_task()
+    )
+
+
+test_pipeline()
