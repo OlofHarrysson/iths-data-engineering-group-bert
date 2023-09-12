45b448acbba3b27e3b05ca6e7e213cee4e78662d
JoelOscarsson <j_oscarsson@hotmail.com>
2023-09-12 13:18:50 +0200

    created timestamp

--- src/newsfeed/count_articles.py
+++ src/newsfeed/count_articles.py
@@ -1,9 +1,40 @@
+import json
 import os
+from datetime import date
+
+
+def extract_timestamps_from_json_files(parent_folder):
+    timestamps = []
+
+    for root, _, files in os.walk(parent_folder):
+        for filename in files:
+            if filename.endswith(".json"):
+                file_path = os.path.join(root, filename)
+
+                try:
+                    with open(file_path, "r") as json_file:
+                        data = json.load(json_file)
+                        published_timestamp = data.get("published")
+
+                        if published_timestamp:
+                            published_date = date.fromisoformat(published_timestamp)
+                            timestamps.append(published_date)
+                        else:
+                            print(f"Warning: No 'published' field in {file_path}")
+                except Exception as e:
+                    print(f"Error processing {file_path}: {str(e)}")
+
+    return timestamps
+
+
+parent_folder = r"data/data_warehouse/articles/"  # Define the parent folder containing the subfolders with JSON files
+timestamps = extract_timestamps_from_json_files(parent_folder)
+
+print(len(timestamps))
+
 
 # count amount of files in folder data_warehouse
 file_count = sum(len(files) for _, _, files in os.walk(r"data/data_warehouse/articles"))
 print(file_count)
-
-
-def compare_articles():
-    pass
+# Will out put first total amount of files in folder with subfolder data_warehouse that is json files
+# second will output total amount of files in folder with subfolder data_warehouse/articles
