d0c5e7ee6f60c91d2e10f84e025bf536db085611
Andreas Svensson <andreas.svensson@iths.se>
2023-09-14 09:35:40 +0200

    implement function to get timestamp

--- src/newsfeed/pipeline_last_ran.py
+++ src/newsfeed/pipeline_last_ran.py
@@ -1,13 +1,31 @@
 import json
 import os
 from datetime import datetime, timedelta
+from pathlib import Path
+
+from newsfeed.get_cached_files import data_directory_path
 
 
-def timestamp_now():
-    with open("data/data_warehouse/timestamp/timestamps.txt", "w") as file:
+def save_timestamp():
+    with open("data/data_warehouse/timestamps.txt", "w") as file:
         time = datetime.now()
         file.write(str(time) + "\n")
 
 
-# Call the function to execute it
-timestamp_now()
+def get_timestamp():
+    timestamp_path = Path(data_directory_path + "pipeline_ran.txt")
+    with open(timestamp_path, "r") as file:
+        text = file.read()
+
+        # parse date and time from the text
+        datetime_obj = datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
+
+        # format date as "yyyy-mm-dd"
+        formatted_timestamp = datetime_obj.strftime("%Y-%m-%d")
+
+    return formatted_timestamp
+
+
+if __name__ == "__main__":
+    # save timestamp to data warehouse (used to see when pipeline was last ran)
+    save_timestamp()
