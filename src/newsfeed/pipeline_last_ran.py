2feafcf0e48705752debc2c63f1c23ca48c337ef
Andreas Svensson <andreas.svensson@iths.se>
2023-09-14 10:07:58 +0200

    implement time_since_pipeline_ran

--- src/newsfeed/pipeline_last_ran.py
+++ src/newsfeed/pipeline_last_ran.py
@@ -7,9 +7,9 @@ from newsfeed.get_cached_files import data_directory_path
 
 
 def save_timestamp():
-    with open("data/data_warehouse/timestamps.txt", "w") as file:
+    with open(data_directory_path + "pipeline_ran.txt", "w") as file:
         time = datetime.now()
-        file.write(str(time) + "\n")
+        file.write(str(time))
 
 
 def get_timestamp():
@@ -17,15 +17,48 @@ def get_timestamp():
     with open(timestamp_path, "r") as file:
         text = file.read()
 
-        # parse date and time from the text
-        datetime_obj = datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
+    timestamp = datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
+    return timestamp
 
-        # format date as "yyyy-mm-dd"
-        formatted_timestamp = datetime_obj.strftime("%Y-%m-%d")
 
-    return formatted_timestamp
+def time_since_pipeline_ran():
+    # datetime_str = "2023-09-14 09:47:48.079068"
+    timestamp = get_timestamp()
+
+    # Get the current datetime
+    current_datetime = datetime.now()
+
+    # Calculate the time difference
+    time_difference = current_datetime - timestamp
+
+    # Extract the number of days, seconds, and microseconds from the time difference
+    days = time_difference.days
+    seconds = time_difference.seconds
+    microseconds = time_difference.microseconds
+
+    # Calculate the total number of seconds
+    total_seconds = days * 24 * 3600 + seconds + microseconds / 1e6
+
+    # Define thresholds for different units of time
+    minute_threshold = 60
+    hour_threshold = 60 * minute_threshold
+    day_threshold = 24 * hour_threshold
+
+    # Format the time difference
+    if total_seconds < minute_threshold:
+        result = f"{int(total_seconds)} seconds ago"
+    elif total_seconds < hour_threshold:
+        result = f"{int(total_seconds / minute_threshold)} minutes ago"
+    elif total_seconds < day_threshold:
+        result = f"{int(total_seconds / hour_threshold)} hours ago"
+    else:
+        result = f"{int(total_seconds / day_threshold)} days ago"
+
+    return result
 
 
 if __name__ == "__main__":
     # save timestamp to data warehouse (used to see when pipeline was last ran)
+    print(time_since_pipeline_ran())
     save_timestamp()
+    print(time_since_pipeline_ran())
